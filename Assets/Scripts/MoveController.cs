7bce0c4f2453de2f6b5a2111d157fdf0d8119e95
Иван Трутнев <trutnev@intervolga.ru>
2024-10-26 22:14:01 +0300

    Добавил скрипт поворота игрока в зависимости от положения мышки

---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    void Start()
+    {
+        // transform.position = new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, transform.position.z);
+    }
+    
+    void Update()
+    {
+        
+    }
+}
