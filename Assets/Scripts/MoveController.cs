a32465c666d23702473fc1bca56c4cbfc0582ecd
Иван Трутнев <trutnev@intervolga.ru>
2024-10-27 17:51:08 +0300

    Добавил скрипт с передвижением персонажа

--- Assets/Scripts/MoveController.cs
+++ Assets/Scripts/MoveController.cs
@@ -2,13 +2,26 @@ using UnityEngine;
 
 public class MoveController : MonoBehaviour
 {
-    void Start()
-    {
-        // transform.position = new Vector3(Screen.width / 2.0f, Screen.height / 2.0f, transform.position.z);
-    }
+    [SerializeField] private float speed = 1;
+    [SerializeField] private CharacterController characterController;
+    [SerializeField] private Camera camera;
     
+    private float horizontalMove;
+    private float verticalMove;
+
     void Update()
     {
-        
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = -Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        float moveX = characterController.transform.position.x; 
+        float moveZ = characterController.transform.position.z; 
+            
+        characterController.Move(new Vector3(verticalMove, Physics.gravity.y, horizontalMove) * Time.deltaTime * speed);
+
+        camera.transform.position += new Vector3(characterController.transform.position.x - moveX, 0, characterController.transform.position.z - moveZ);
     }
 }
--- Assets/Scripts/RotationController.cs
+++ Assets/Scripts/RotationController.cs
@@ -1,44 +1,12 @@
 ﻿using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class RotationController : MonoBehaviour
 {
-	private Transform transform;
-	private Vector2 vec;
-	[SerializeField] private LayerMask groundMask;
-
-	void Start()
-	{
-		transform = GetComponent<Transform>();
-		vec = new Vector2(Screen.width / 2, Screen.height / 2);
-		Debug.Log("(" + Screen.width + ", " + Screen.height + ")");
-	}
-
 	void Update()
-	{
-		Aim();
-	}
-
-	private void Aim()
 	{
 		Vector3 mp = Input.mousePosition;
 		float angle = Mathf.Atan2(Screen.height / 2.0f - mp.y, Screen.width / 2.0f - mp.x);
-		Debug.Log(180 - angle * Mathf.Rad2Deg);
-
-		
-		
-		// transform.rotation = Quaternion.Euler(
-		// 	Vector3.Lerp(
-		// 		transform.rotation.eulerAngles,
-		// 		new Vector3(transform.eulerAngles.x, 180 - angle * Mathf.Rad2Deg, transform.eulerAngles.z),
-		// 		10 * Time.deltaTime)
-		// 	); 
-			
-		
-		// transform.rotation =
-			// Quaternion.Euler(transform.eulerAngles.x, 180 - angle * Mathf.Rad2Deg, transform.eulerAngles.z)
 		transform.rotation =
 			Quaternion.Euler(transform.eulerAngles.x, Mathf.LerpAngle(transform.eulerAngles.y, 180 - angle * Mathf.Rad2Deg, 10 * Time.deltaTime), transform.eulerAngles.z);
-
 	}
 }
