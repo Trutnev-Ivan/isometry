a03a44c5dbe47e389df7fb6e7e2c7dd854bcd58a
Иван Трутнев <trutnev@intervolga.ru>
2024-10-27 21:53:10 +0300

    Добавил логику с прицеливанием

---
+++ Assets/Scripts/AimController.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class AimController: MonoBehaviour
+{
+	[SerializeField] private CharacterController characterController;
+	[SerializeField] private LayerMask layerMask;
+	[SerializeField] private Transform topBodyOriginRotation;
+	[SerializeField] private Transform botBodyOriginRotation;
+
+	private bool needInitRotation = false;
+	private RaycastHit wallHit;
+	private bool isAimBtnPressed = false;
+
+	public void Update()
+	{
+		isAimBtnPressed = Input.GetAxisRaw("Fire2") != 0;
+	}
+
+	public void FixedUpdate()
+	{
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		if (!isAimBtnPressed)
+		{
+			resetAim();
+			return;
+		}
+		
+		if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
+		{
+			aim(ref hit);
+		}
+		else if (hit.point == Vector3.zero && needInitRotation)
+		{
+			resetAim();
+		}
+	}
+
+	protected void aim(ref RaycastHit hit)
+	{
+		topBodyOriginRotation.LookAt(hit.point);
+
+		if (Physics.Linecast(characterController.transform.position, hit.point, out hit, layerMask))
+		{
+			wallHit = hit;
+		}
+			
+		Debug.DrawLine(characterController.transform.position, wallHit.point, Color.red);
+			
+		needInitRotation = true;
+	}
+	
+	protected void resetAim()
+	{
+		topBodyOriginRotation.forward = botBodyOriginRotation.forward;
+		needInitRotation = false;
+	}
+	
+}
